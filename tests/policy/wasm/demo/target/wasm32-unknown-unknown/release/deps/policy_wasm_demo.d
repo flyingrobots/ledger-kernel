5c869e808c3d09dc0d61badbdc9f4f073f685177
J. Kirby Ross <james@flyingrobots.dev>
2025-11-05 13:25:42 -0800

    chore(wasm-demo): remove committed build artifacts; ignore target/ and Cargo.lock for demo crate

--- tests/policy/wasm/demo/target/wasm32-unknown-unknown/release/deps/policy_wasm_demo.d
+++
@@ -1,5 +0,0 @@
-/Users/james/git/ledger-kernel/tests/policy/wasm/demo/target/wasm32-unknown-unknown/release/deps/policy_wasm_demo.d: src/lib.rs
-
-/Users/james/git/ledger-kernel/tests/policy/wasm/demo/target/wasm32-unknown-unknown/release/deps/policy_wasm_demo.wasm: src/lib.rs
-
-src/lib.rs:
--- tests/policy/wasm/demo/target/wasm32-unknown-unknown/release/policy_wasm_demo.d
+++
@@ -1,1 +0,0 @@
-/Users/james/git/ledger-kernel/tests/policy/wasm/demo/target/wasm32-unknown-unknown/release/policy_wasm_demo.wasm: /Users/james/git/ledger-kernel/tests/policy/wasm/demo/src/lib.rs
